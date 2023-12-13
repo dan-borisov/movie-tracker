ec8c54497cdcb0c8dae58ef50f5414307c29fd28
dan-borisov <mrborisov1@gmail.com>
2023-12-13 10:44:21 +0000

    added similar movies section to singleMovie component, added fallback placeholders for posters

---
+++ src/components/SimilarMovies.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { TMDB_API_BASE_URL, TMDB_API_KEY } from '../config.js';
+
+function slugify(title) {
+  return title.toLowerCase().replace(/[^a-zA-Z ]/g, '').replace(/\s+/g, '-');
+}
+
+
+function SimilarMovies({ movieId }) {
+    const [movies, setMovies] = useState([]);
+  
+    useEffect(() => {
+      const fetchMovies = async () => {
+        try {
+          let endpoint = `${TMDB_API_BASE_URL}/movie/${movieId}/similar?api_key=${TMDB_API_KEY}`;
+          const response = await fetch(endpoint);
+          const data = await response.json();
+          setMovies(data.results);
+        } catch (error) {
+          console.error('Error fetching data from TMDB', error);
+        }
+      };
+  
+      fetchMovies();
+    }, [movieId]);
+  
+    return (
+      <div>
+        <div className="container mt-3">
+          <h2 className='mb-2'>Similar Movies</h2>
+          <div className="row gy-4">
+            {movies.map((movie) => {
+              const posterPath = movie.poster_path
+                ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
+                : process.env.PUBLIC_URL + '/poster-placeholder.png';
+  
+              return (
+                <div key={movie.id} className="col-6 col-md-4 col-lg-2">
+                  <Link to={`/movie/${movie.id}/${slugify(movie.title)}`}>
+                    <img className="movieCard img-fluid rounded" src={posterPath} alt={movie.title} />
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default SimilarMovies;
+  
--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import SimilarMovies from './SimilarMovies';
 import { TMDB_API_BASE_URL, TMDB_API_KEY, TMDB_API_IMAGE_URL } from '../config';
 import '../styles/SingleMovie.css';
 
@@ -52,40 +53,44 @@ function SingleMovie() {
     return <p>Loading...</p>;
   }
 
+  const posterPath = movie.poster_path 
+    ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
+    : process.env.PUBLIC_URL + '/poster-placeholder.png';
+
   return (
     <div>
         <div className="container-flex backdrop-container">
             <img className='movie-backdrop' src={TMDB_API_IMAGE_URL + movie.backdrop_path} alt={movie.title} />
         </div>
-        <div className="movie-box p-3 rounded">
+        <div className="movie-box p-3">
             <div className="container">
                 <div className="row">
                     <div className="col-md-4 d-none d-md-block">
-                        <img className="img-fluid rounded" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+                        <img className="img-fluid rounded" src={posterPath} alt={movie.title} />
                     </div>
                     <div className="col-md-8">
                         <h1>{movie.title}</h1>
-                        <p className="movie-year"> ({movie.release_date.slice(0, 4)})</p>
+                        <p className="movie-year"> ({movie.release_date ? movie.release_date.slice(0, 4) : 'Unknown'})</p>
                         {movie.genres.map((genreObj) => (
                             <span key={genreObj.name} className='badge bg-warning text-dark mx-2'>
                                 {genreObj.name}
                             </span>
                         ))}
                         <div className="container-fluid px-0">
-                            <div className="row justify-content-center align-items-center mx-0 my-5">
-                                <div className="col">
+                            <div className="row justify-content-center align-items-center mx-0 my-3">
+                                <div className="col-lg-4 gy-2">
                                     <span className='details-span'>Runtime: </span>
-                                    <span>{Math.floor(movie.runtime / 60)}h {movie.runtime % 60}min</span>
+                                    <span>{movie.runtime ? `${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}min` : 'Unknown'}</span>
                                 </div>
-                                <div className="col">
+                                <div className="col-lg-4 gy-2">
                                     <span className='details-span'>Age Rating: </span>
                                     <span>{ageRating}</span>
                                 </div>
-                                <div className="col">
+                                <div className="col-lg-4 gy-2">
                                     <span>
                                         <img src={process.env.PUBLIC_URL + '/imdb-logo.png'} alt="imdb logo" className='imdb-logo'/>
                                     </span>
-                                    <span style={{ verticalAlign: 'middle' }}> {(Math.round(movie.vote_average * 10) /10).toFixed(1)}</span>
+                                    <span style={{ verticalAlign: 'middle' }}> { movie.vote_average ? (Math.round(movie.vote_average * 10) /10).toFixed(1) : 'Unknown'}</span>
                                 </div>
                             </div>
                         </div>
@@ -93,6 +98,9 @@ function SingleMovie() {
                     </div>
                 </div>
             </div>
+        </div>
+        <div className="container">
+          <SimilarMovies movieId={id}/>                  
         </div>
     </div>
   );
