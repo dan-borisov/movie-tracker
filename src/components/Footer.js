e8c636e1ddd7c957c90ff3c8f796ddeb06b30c0e
dan-borisov <mrborisov1@gmail.com>
2023-12-14 18:53:52 +0000

    prettyfied footer, added placeholders, added Random Movie button to navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,74 @@
 import React from 'react';
+import '../styles/Footer.css';
+import ARandomButton from './ARandomButton';
 
 function Footer() {
     return (
         <footer>
-            <div class="container mt-5">
-                <div class="row">
+            <div class="container mt-5 py-5">
+                <div class="row gy-5">
                     <div class="col-12 col-lg-4">
-                        <img
-                        alt=""
-                        src="logo.jpg"
-                        width="200"
-                        height="75"
-                        className="d-inline-block align-top"
-                        />
+                        <h2 className='footer-heading'>MovieTracker</h2>
+                        <p className='footer-slogan'>Explore your favourite movies and TV Shows</p>
+                        <ARandomButton />
                     </div>
-                    <div class="col-12 col-md-6 col-lg-2">
-                        <h4>Categories</h4>
+                    <div class="col-6 text-center col-lg-2">
+                        <h4>Popular</h4>
                         <ul>
-                            <li>List Item One</li>
-                            <li>List Item Two</li>
-                            <li>List Item three</li>
-                            <li>List Item Four</li>
+                            <li>
+                                <a href="/">Action</a>
+                            </li>
+                            <li>
+                                <a href="/">Drama</a>
+                            </li>
+                            <li>
+                                <a href="/">Thriller</a>
+                            </li>
+                            <li>
+                                <a href="/">Comedy</a>
+                            </li>
                         </ul>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-2">
-                        <h4>Categories</h4>
+                    <div class="col-6 text-center col-lg-2">
+                        <h4>Social Media</h4>
                         <ul>
-                            <li>List Item One</li>
-                            <li>List Item Two</li>
-                            <li>List Item three</li>
-                            <li>List Item Four</li>
+                            <li>
+                                <a href="/">Twitter</a>
+                            </li>
+                            <li>
+                                <a href="/">Facebook</a>
+                            </li>
+                            <li>
+                                <a href="/">Instagram</a>
+                            </li>
+                            <li>
+                                <a href="/">Pinterest</a>
+                            </li>
                         </ul>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-2">
-                        <h4>Categories</h4>
+                    <div class="col-6 text-center col-lg-2">
+                        <h4>Company</h4>
                         <ul>
-                            <li>List Item One</li>
-                            <li>List Item Two</li>
-                            <li>List Item three</li>
-                            <li>List Item Four</li>
+                            <li>
+                                <a href="/">About Us</a>
+                            </li>
+                            <li>
+                                <a href="/">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="/">Careers</a>
+                            </li>
                         </ul>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-2">
-                        <h4>Categories</h4>
+                    <div class="col-6 text-center col-lg-2">
+                        <h4>Legal</h4>
                         <ul>
-                            <li>List Item One</li>
-                            <li>List Item Two</li>
-                            <li>List Item three</li>
-                            <li>List Item Four</li>
+                            <li>
+                                <a href="/">Terms of Use</a>
+                            </li>
+                            <li>
+                                <a href="/">Privacy Policy</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Header.css';
+import ARandomButton from './ARandomButton';
+import { useLocation } from 'react-router-dom';
 
 
 function Header() {
   const [backgroundColor, setBackgroundColor] = useState();
+  const [showRandomButton, setShowRandomButton] = useState(false);
+  const location = useLocation();
+
+  
 
   useEffect(() => {
+    if (location.pathname !== '/') {
+      setShowRandomButton(true);
+    }
     const handleScroll = () => {
+        const isHomePage = location.pathname === '/';
+        const isScrolledPast500 = window.scrollY > 500;
       
-        if (window.scrollY > 500) {
-          setBackgroundColor('#060D17');
+
+        if (isScrolledPast500) {
+          setBackgroundColor('#060D17'); 
         } else {
-          setBackgroundColor('');
+          setBackgroundColor(''); 
         }
-      
+
+        setShowRandomButton((!isHomePage) || isHomePage && isScrolledPast500);
     };
+    
     window.addEventListener('scroll', handleScroll);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
     }
-  }, []);
+  }, [location.pathname]);
+
     return (
       <nav class="navbar fixed-top navbar-light" style={{ backgroundColor, transition: 'background-color 0.4s ease' }}>
         <div class="container">
           <a class="navbar-brand" href="/">MovieTracker</a>
+          { showRandomButton && <ARandomButton /> }
         </div>
       </nav>
     );
