0f7e4f282dae302012d0175a356d8404ce61a15a
dan-borisov <mrborisov1@gmail.com>
2023-12-16 12:50:37 +0000

    added singleMovie and singleShow pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
 import SingleMovie from './pages/SingleMovie';
+import SingleShow from './pages/SingleShow'
 import TvShows from './pages/TvShows'
 import Movies from './pages/Movies'
 
@@ -20,7 +21,8 @@ function App() {
             path="/"
             element={<HomePage />}
           />
-          <Route path="/:type/:id/:title" element={<SingleMovie />} />
+          <Route path="/movie/:id/:title" element={<SingleMovie />} />
+          <Route path="/tv-show/:id/:title" element={<SingleShow />} />
           <Route path="/movies" exact element={<Movies />}/>
           <Route path="/tv-shows" exact element={<TvShows />}/>
         </Routes>
--- src/pages/SingleShow.js
+++ src/pages/SingleShow.js
@@ -7,7 +7,7 @@ import '../styles/SingleMovie.css';
 
 function SingleMovie() {
   const { id } = useParams();
-  const [movie, setMovie] = useState(null);
+  const [show, setShow] = useState(null);
   const [ageRating, setAgeRating] = useState(null);
 
 
@@ -15,10 +15,10 @@ function SingleMovie() {
     window.scrollTo(0, 0);
     const fetchMovieDetails = async () => {
       try {
-        const endpoint = `${TMDB_API_BASE_URL}/movie/${id}?api_key=${TMDB_API_KEY}&include_adult=false`;
+        const endpoint = `${TMDB_API_BASE_URL}/tv/${id}?api_key=${TMDB_API_KEY}&include_adult=false`;
         const response = await fetch(endpoint);
         const data = await response.json();
-        setMovie(data);
+        setShow(data);
         console.log(data);
       } catch (error) {
         console.error('Error fetching movie details', error);
@@ -31,11 +31,11 @@ function SingleMovie() {
   useEffect(() => {
     const fetchAgeRating = async () => {
       try {
-        const endpoint = `${TMDB_API_BASE_URL}/movie/${id}/release_dates?api_key=${TMDB_API_KEY}`;
+        const endpoint = `${TMDB_API_BASE_URL}/tv/${id}/content_ratings?api_key=${TMDB_API_KEY}`;
         const response = await fetch(endpoint);
         const data = await response.json();
-        const releaseDates = data.results.find(item => item.iso_3166_1 === "US");
-        const age = releaseDates ? releaseDates.release_dates[0].certification : 'Unknown';
+        const rating = data.results.find(item => item.iso_3166_1 === "US");
+        const age = rating ? rating.rating : 'Unknown';
         const ageRating = age ? age : 'Unknown';
         
         setAgeRating(ageRating);
@@ -47,37 +47,45 @@ function SingleMovie() {
     fetchAgeRating();
   }, [id]);
 
-  if (!movie) {
+  if (!show) {
     return <p>Loading...</p>;
   }
 
-  if (!ageRating) {
-    return <p>Loading...</p>;
-  }
-
-  const posterPath = movie.poster_path 
-    ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
+  
+  const posterPath = show.poster_path 
+    ? `https://image.tmdb.org/t/p/w342${show.poster_path}`
     : process.env.PUBLIC_URL + '/poster-placeholder.png';
 
-  const backdropPath = movie.backdrop_path
-    ? (TMDB_API_IMAGE_URL + movie.backdrop_path)
+  const backdropPath = show.backdrop_path
+    ? (TMDB_API_IMAGE_URL + show.backdrop_path)
     : process.env.PUBLIC_URL + '/movie-tracker-hero.jpg';
+
+  function airDates() {
+    
+    if (show.first_air_date && show.last_air_date) {
+        if(show.in_production) {
+            return `${show.first_air_date.slice(0, 4)} - present`
+        }
+        return `${show.first_air_date.slice(0, 4)} - ${show.last_air_date.slice(0, 4)}`
+    }
+      return 'Unknown'
+    }
 
   return (
     <div>
         <div className="container-flex backdrop-container">
-            <img className='movie-backdrop' src={backdropPath} alt={movie.title} />
+            <img className='movie-backdrop' src={backdropPath} alt={show.name} />
         </div>
         <div className="movie-box p-3">
             <div className="container">
                 <div className="row">
                     <div className="col-md-4 d-none d-md-block">
-                        <img className="img-fluid rounded" src={posterPath} alt={movie.title} />
+                        <img className="img-fluid rounded" src={posterPath} alt={show.name} />
                     </div>
                     <div className="col-md-8">
-                        <h1>{movie.title}</h1>
-                        <p className="movie-year"> ({movie.release_date ? movie.release_date.slice(0, 4) : 'Unknown'})</p>
-                        {movie.genres.map((genreObj) => (
+                        <h1>{show.name}</h1>
+                        <p className="movie-year"> ({airDates()})</p>
+                        {show.genres.map((genreObj) => (
                             <span key={genreObj.name} className='badge bg-warning text-dark mx-2'>
                                 {genreObj.name}
                             </span>
@@ -85,8 +93,8 @@ function SingleMovie() {
                         <div className="container-fluid px-0">
                             <div className="row justify-content-center align-items-center mx-0 my-3">
                                 <div className="col-lg-4 gy-2">
-                                    <span className='details-span'>Runtime: </span>
-                                    <span>{movie.runtime ? `${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}min` : 'Unknown'}</span>
+                                    <span className='details-span'>Seasons: </span>
+                                    <span>{show.number_of_seasons || 'Unknown'}</span>
                                 </div>
                                 <div className="col-lg-4 gy-2">
                                     <span className='details-span'>Age Rating: </span>
@@ -96,11 +104,11 @@ function SingleMovie() {
                                     <span>
                                         <img src={process.env.PUBLIC_URL + '/imdb-logo.png'} alt="imdb logo" className='imdb-logo'/>
                                     </span>
-                                    <span style={{ verticalAlign: 'middle' }}> { movie.vote_average ? (Math.round(movie.vote_average * 10) /10).toFixed(1) : 'Unknown'}</span>
+                                    <span style={{ verticalAlign: 'middle' }}> { show.vote_average ? (Math.round(show.vote_average * 10) /10).toFixed(1) : 'Unknown'}</span>
                                 </div>
                             </div>
                         </div>
-                        <p>{movie.overview}</p>
+                        <p>{show.overview}</p>
                     </div>
                 </div>
             </div>
